78143c963213e69f469abc0dbcbb6a2fd0646e27
natsumendoza <mendozalaxus@yahoo.com>
2016-07-01 14:01:45 +0800

    first commit

---
+++ RESTServicesSpring/src/test/java/org/ws/RestServicesSpringApplicationTests.java
@@ -0,0 +1,18 @@
+package org.ws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RestServicesSpringApplication.class)
+@WebAppConfiguration
+public class RestServicesSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
